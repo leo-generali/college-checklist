0184fde411fd69d3d49db38393bb7b361b6261c8
Leo Generali <leogen17@gmail.com>
2017-04-18 22:16:31 -0400

    Create working AJAX function. Returns all the right data

--- src/helpers/xmlhttprequest.js
+++ src/helpers/xmlhttprequest.js
@@ -1,23 +1,23 @@
 import api from './api.js';
 import { urlCreator } from './url_creator.js';
 
-export function getJSON(url) {
+export function getJSON(name) {
 
   const mods = urlCreator();
 
-  console.log(
+  const url = 
     api.prefix + 
-    url + "&_fields=" +
+    name + "&_fields=" +
     mods +
-    api.key
-  );
+    api.key;
 
-  console.log(mods);
+  console.log(url);
 
   var request = new XMLHttpRequest();
   
   request.onreadystatechange = function() {
     if (this.readyState === 4 && this.status === 200) {
+      console.log(JSON.parse(this.responseText)); 
       return JSON.parse(this.responseText); 
     }
   };
