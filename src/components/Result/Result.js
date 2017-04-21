48e108a460bcf7d5ddde394c8adc5ddff29d630f
Leo Generali <leogen17@gmail.com>
2017-04-21 16:40:00 -0400

    Display the rest of the school's info. Still gotta make it look good. May add other stuff

--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -9,6 +9,7 @@ class Result extends Component {
     //School Information
     const schoolName = school["school.name"];
     const schoolLocation = school["school.city"] + ", " + school["school.state"];
+    const schoolPublic = school[ year + ".cost.avg_net_price.public"] ? "public" : "private";
 
     //Demographics Info
     const schoolSize = school[ year + ".student.size"];
@@ -17,11 +18,20 @@ class Result extends Component {
     const schoolSizeMenAbsolute = schoolSizeMenPercent * schoolSize;
     const schoolSizeWomenAbsolute = schoolSizeWomenPercent * schoolSize;
 
-
+    //Price Info
+    const schoolPrice = school[ year + ".cost.attendance.academic_year"];
+    const schoolPriceAvg = school[ year + ".cost.avg_net_price.public"] ? school[ year + ".cost.avg_net_price.public"] : school[ year + ".cost.avg_net_price.private"];
+    
+    //Admissions Info
+    const schoolAdmitRate = school[ year + ".admissions.admission_rate.overall"];
+    const schoolAdmitSAT = school[ year + ".admissions.sat_scores.average.overall"];
+    const schoolCompRate = school[ year + ".completion.completion_rate_4yr_150nt"] ? school[ year + ".completion.completion_rate_4yr_150nt"] : school[ year + ".completion.completion_rate_less_than_4yr_150nt"];
+    
     return (  
       <div className="results">
         <h3> { schoolName } </h3>
         <p> { schoolLocation } </p>
+        <p> { schoolPublic } </p>
         <div className="results__seperatorWrapper">
           <span className="results__seperatorblock"/>
           <p>Demographics Info</p>
@@ -32,6 +42,22 @@ class Result extends Component {
         <p> { schoolSizeWomenPercent } </p>
         <p> { schoolSizeMenAbsolute } </p>
         <p> { schoolSizeWomenAbsolute } </p>
+        <div className="results__seperatorWrapper">
+          <span className="results__seperatorblock"/>
+          <p>Cost Info</p>
+          <span className="results__seperatorblock"/>
+        </div>
+        <p> { schoolPrice } </p>
+        <p> { schoolPriceAvg } </p>
+        <div className="results__seperatorWrapper">
+          <span className="results__seperatorblock"/>
+          <p>Academic Info</p>
+          <span className="results__seperatorblock"/>
+        </div>
+        <p> { schoolAdmitRate } </p>
+        <p> { schoolAdmitSAT } </p>
+        <p> { schoolCompRate } </p>
+
       </div>
     )
   }
